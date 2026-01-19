1b855f86b11fbd3d76e784357357a8d9fc2b8d68
adrian maciuc <adrian.maciuc@gmail.com>
2026-01-19 23:23:46 +0200

    update cors again 3

--- config/server.ts
+++ config/server.ts
@@ -1,8 +1,8 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', process.env.NODE_ENV === 'production' ? 3000 : 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", process.env.NODE_ENV === "production" ? 3000 : 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
-  url: env('PUBLIC_URL', undefined),
+  url: env("PUBLIC_URL", undefined),
 });
