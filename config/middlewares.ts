8f01fcf542b1263d1f0ef71a7e79515dafd15799
adrian maciuc <adrian.maciuc@gmail.com>
2026-01-19 22:44:46 +0200

    update cors again

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -5,7 +5,9 @@ export default [
   {
     name: "strapi::cors",
     config: {
-      origin: ["https://apexshoes.martioli.com/"],
+      origin: ["https://apexshoes.martioli.com/", "http://localhost:5173"],
+      credentials: true,
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
     },
   },
   "strapi::poweredBy",
